46a4fdac7367082096fe08021db2911b4110efcb
Maria Ortega <mariaortegapb@gmail.com>
2021-04-11 02:28:04 -0400

    testing the test files

--- Develop/test/Engineer.test.js
+++ Develop/test/Engineer.test.js
@@ -3,12 +3,12 @@ const Engineer = require("../lib/Engineer");
 test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.github).toBe(testValue);
+  expect(e.getGitHubUser()).toBe(testValue);
 });
 
-test("getRole() should return \"Engineer\"", () => {
+test('getRole() should return "Engineer"', () => {
   const testValue = "Engineer";
-  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser", testValue);
   expect(e.getRole()).toBe(testValue);
 });
 
--- Develop/test/Intern.test.js
+++ Develop/test/Intern.test.js
@@ -3,12 +3,12 @@ const Intern = require("../lib/Intern");
 test("Can set school via constructor", () => {
   const testValue = "UCLA";
   const e = new Intern("Foo", 1, "test@test.com", testValue);
-  expect(e.school).toBe(testValue);
+  expect(e.getSchool()).toBe(testValue);
 });
 
-test("getRole() should return \"Intern\"", () => {
+test('getRole() should return "Intern"', () => {
   const testValue = "Intern";
-  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
   expect(e.getRole()).toBe(testValue);
 });
 
